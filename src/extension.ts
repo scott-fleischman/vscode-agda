f9a25a02af3441754c11ab86ac6c4714fa9bc7ed
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-11-05 21:05:42 -0600

    Modify word pattern to support input-assist better

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as client from "./client";
 import * as vscode from "vscode";
 
 const agdaConfiguration = {
-  wordPattern: /[^\\\s\d(){}#][^\s(){}]*/,
+  wordPattern: /\\[^\s]+|[^\\\s\d(){}#][^\s(){}]*/,
 };
 
 export async function activate(context: vscode.ExtensionContext) {
